4be31b7337a8d0ed593ab1a7efa424da8dc4f379
Alessandro Martins dos Santos <alessandromartinsdosantos@gmail.com>
2025-05-16 20:25:47 -0300

    16/05

---
+++ JavaScript Utilizando tipos, variaveis e funcoes/01-variaveis.js
@@ -0,0 +1,8 @@
+const estudante = "Caroline"; // Não pode ser alterado durante o codigo 
+let professora = "Ana"; // Pode ser alterado durante o código (Usar em casos específicos)
+
+console.log(estudante, professora);
+
+//Template Literals ou template strings
+const nome = "Alice";
+console.log(`Olá, meu nome é ${nome}`);
---
+++ JavaScript Utilizando tipos, variaveis e funcoes/02-escopo.js
@@ -0,0 +1,7 @@
+if (true){
+    let estudante = "Caroline";
+    console.log(estudante);
+}
+
+estudante = "Ana";
+console.log(estudante);
